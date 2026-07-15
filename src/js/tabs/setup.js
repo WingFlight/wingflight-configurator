f4810dbe542b5db05c875226aef8437b1751f1a7
Rob Thomson <rob@rt-rc.com>
2026-07-15 22:06:12 +0100

    Revert "Convert Setup tab to Svelte"
    This reverts commit d0d720b85d327afdb3cf7e3fc9e7114516aa12e1.

--- src/js/tabs/setup.js
+++ src/js/tabs/setup.js
@@ -1,26 +1,163 @@
-import { mount, unmount } from "svelte";
-
-import Setup from "@/tabs/setup/Setup.svelte";
-
 const tab = {
-    tabName: "setup",
-    svelteComponent: null,
+    tabName: 'setup',
+};
 
-    initialize(callback) {
-        const target = document.querySelector("#content");
-        target.innerHTML = "";
-        this.svelteComponent = mount(Setup, { target });
+tab.initialize = function (callback) {
+    load_data(load_html);
+
+    function load_data(callback) {
+        Promise.resolve(true)
+            .then(() => MSP.promise(MSPCodes.MSP_STATUS))
+            .then(() => MSP.promise(MSPCodes.MSP_ARMING_CONFIG))
+            .then(() => MSP.promise(MSPCodes.MSP_FEATURE_CONFIG))
+            .then(() => MSP.promise(MSPCodes.MSP_ADVANCED_CONFIG))
+            .then(callback);
+    }
+
+    function load_html() {
+        $('#content').load("/src/tabs/setup.html", process_html);
+    }
+
+    function process_html() {
+
+        // translate to user-selected language
+        i18n.localizePage();
+
+        // saving and uploading an imaginary config to hardware is a bad idea
+        if (CONFIGURATOR.virtualMode) {
+            $('a.backupSettings').addClass('disabled');
+        }
+
+        // check if we have accelerometer and magnetometer
+        if (!have_sensor(FC.CONFIG.activeSensors, 'acc')) {
+            $('a.calibrateAccel').addClass('disabled');
+            $('default_btn').addClass('disabled');
+        }
+
+        if (!have_sensor(FC.CONFIG.activeSensors, 'mag')) {
+            $('a.calibrateMag').addClass('disabled');
+            $('default_btn').addClass('disabled');
+        }
+
+        $('a.rebootBootloader').click(function () {
+            MSP.send_message(MSPCodes.MSP_SET_REBOOT, [ mspHelper.REBOOT_TYPES.BOOTLOADER ], false);
+        });
+
+        // UI Hooks
+        $('a.calibrateAccel').click(function () {
+            const _self = $(this);
+
+            if (!_self.hasClass('calibrating')) {
+                _self.addClass('calibrating');
+
+                // During this period MCU won't be able to process any serial commands because its locked in a for/while loop
+                // until this operation finishes, sending more commands through data_poll() will result in serial buffer overflow
+                GUI.interval_pause('setup_data_pull');
+
+                MSP.send_message(MSPCodes.MSP_ACC_CALIBRATION, false, false, function () {
+                    GUI.log(i18n.getMessage('initialSetupAccelCalibStarted'));
+                    $('#accel_calib_running').show();
+                    $('#accel_calib_rest').hide();
+                });
+
+                GUI.timeout_add('button_reset', function () {
+                    GUI.interval_resume('setup_data_pull');
+                    GUI.log(i18n.getMessage('initialSetupAccelCalibEnded'));
+                    _self.removeClass('calibrating');
+                    $('#accel_calib_running').hide();
+                    $('#accel_calib_rest').show();
+                }, 2000);
+            }
+        });
+
+        $('a.calibrateMag').click(function () {
+            const _self = $(this);
+
+            if (!_self.hasClass('calibrating') && !_self.hasClass('disabled')) {
+                _self.addClass('calibrating');
+
+                MSP.send_message(MSPCodes.MSP_MAG_CALIBRATION, false, false, function () {
+                    GUI.log(i18n.getMessage('initialSetupMagCalibStarted'));
+                    $('#mag_calib_running').show();
+                    $('#mag_calib_rest').hide();
+                });
+
+                GUI.timeout_add('button_reset', function () {
+                    GUI.log(i18n.getMessage('initialSetupMagCalibEnded'));
+                    _self.removeClass('calibrating');
+                    $('#mag_calib_running').hide();
+                    $('#mag_calib_rest').show();
+                }, 30000);
+            }
+        });
+
+        const dialogConfirmReset = $('.dialogConfirmReset')[0];
+
+        $('a.resetSettings').click(function () {
+            dialogConfirmReset.showModal();
+        });
+
+        $('.dialogConfirmReset-cancelbtn').click(function() {
+            dialogConfirmReset.close();
+        });
+
+        $('.dialogConfirmReset-confirmbtn').click(function() {
+            dialogConfirmReset.close();
+            MSP.send_message(MSPCodes.MSP_RESET_CONF, false, false, function () {
+                GUI.log(i18n.getMessage('initialSetupSettingsRestored'));
+                GUI.tab_switch_reload();
+            });
+        });
+
+        $('a.saveSettings').click(function () {
+            MSP.send_message(MSPCodes.MSP_EEPROM_WRITE, false, false, function () {
+                GUI.log(i18n.getMessage('initialSetupSettingsSaved'));
+                GUI.tab_switch_reload();
+            });
+        });
+
+        $('a.backupSettings').click(function () {
+            if ($(this).hasClass('disabled')) {
+                return;
+            }
+
+            configuration_backup(function () {
+                GUI.log(i18n.getMessage('initialSetupBackupSuccess'));
+            });
+        });
+
+        $('a.restoreSettings').click(function () {
+            if ($(this).hasClass('disabled')) {
+                return;
+            }
+
+            configuration_restore(function () {
+                GUI.log(i18n.getMessage('initialSetupRestoreSuccess'));
+                GUI.tab_switch_reload();
+            });
+        });
+
+        $('a.rebootMsc').click(function () {
+            // Reboot into MSC using UTC time offset instead of user timezone
+            // Linux seems to expect that the FAT file system timestamps are UTC based
+            const code = (GUI.operating_system === "Linux") ?
+                mspHelper.REBOOT_TYPES.MSC_UTC :
+                mspHelper.REBOOT_TYPES.MSC ;
+                MSP.send_message(MSPCodes.MSP_SET_REBOOT, [ code ], false);
+        });
+
+        $('a.rebootFirmware').click(function () {
+            GUI.log(i18n.getMessage('deviceRebooting'));
+            MSP.send_message(MSPCodes.MSP_SET_REBOOT, [ mspHelper.REBOOT_TYPES.FIRMWARE ], false);
+            reinitialiseConnection();
+        });
 
         GUI.content_ready(callback);
-    },
+    }
+};
 
-    cleanup(callback) {
-        if (this.svelteComponent) {
-            unmount(this.svelteComponent);
-            this.svelteComponent = null;
-        }
-        callback?.();
-    },
+tab.cleanup = function (callback) {
+    callback?.();
 };
 
 TABS[tab.tabName] = tab;
