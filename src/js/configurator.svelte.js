9cfe089eb900492b97ff6951f51a4b9dbec193e9
Rob Thomson <rob@rt-rc.com>
2026-09-25 00:12:38 +0100

    Require MSP API 22.5 for configuration

--- src/js/configurator.svelte.js
+++ src/js/configurator.svelte.js
@@ -5,7 +5,7 @@ export const API_VERSION_22_3 = "22.3.0";
 export const API_VERSION_22_4 = "22.4.0";
 export const API_VERSION_22_5 = "22.5.0";
 
-export const API_VERSION_WGFL_MIN = API_VERSION_22_0;
+export const API_VERSION_WGFL_MIN = API_VERSION_22_5;
 export const API_VERSION_WGFL_MAX = API_VERSION_22_5;
 
 export const FW_VERSION_WGFL_MIN = "4.3.0-0";
